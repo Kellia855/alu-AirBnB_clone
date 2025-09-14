82c1b87f0c659458d14f03eb03e2a5eb4dfa1d95
Nabide Yusuf <y.nabide@alustudent.com>
2025-09-14 20:48:16 +0200

    changed models to models.engine

--- tests/test_models/console.py
+++ tests/test_models/console.py
@@ -6,12 +6,12 @@ import cmd
 import sys
 from models.engine import storage
 from models.engine.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.engine.user import User
+from models.engine.state import State
+from models.engine.city import City
+from models.engine.amenity import Amenity
+from models.engine.place import Place
+from models.engine.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
